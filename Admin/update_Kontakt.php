3bb5c68118410b9c33693ec78b1669bb3f974758
Uranik Sejdiu <u.sejdiu4@gmail.com>
2020-12-29 12:44:09 +0100

    shtimi, editi i kontaktit pa fshirje

--- Admin/update_Kontakt.php
+++ Admin/update_Kontakt.php
@@ -4,16 +4,68 @@ Author URL: http://w3layouts.com
 License: Creative Commons Attribution 3.0 Unported
 License URL: http://creativecommons.org/licenses/by/3.0/
 -->
-<?php include_once('config.php'); ?>
+<?php include("check.php"); ?>
+<?php include_once("config.php");
+if(isset($_POST['update']))
+{	
+	$ID_Kontakti = $_POST['ID_Kontakti'];
+	
+	$emri=$_POST['Emri'];
+	$email=$_POST['Email'];
+	$tel=$_POST['Tel'];
+	$mesazhi=$_POST['Mesazhi'];
+	
+	// checking empty fields
+	if(empty($emri) || empty($email) || empty($tel) || empty($mesazhi)) {	
+			
+		if(empty($emri)) {
+			echo "<font color='red'>Emri është e zbrazur..</font><br/>";
+		}
+		
+		if(empty($email)) {
+			echo "<font color='red'>Email është e zbrazur.</font><br/>";
+		}
+		
+		if(empty($tel)) {
+			echo "<font color='red'>Nr. Telefonit është e zbrazur.</font><br/>";
+		}
+		if(empty($mesazhi)) {
+			echo "<font color='red'>Mesazhi është e zbrazur.</font><br/>";
+		}		
+	} else {	
+		//updating the table
+		$result = mysqli_query($conn,"CALL uKontakt('$ID_Kontakti','$emri','$email','$tel','$mesazhi')");
+		
+		
+		header("Location: modifiko_Kontakt.php");
+	}
+}
+?>
+<?php
+//getting uid from url
+$ID_Kontakti = $_GET['ID_Kontakti'];
+
+//selecting data associated with this particular uid
+$result = mysqli_query($conn,"CALL PROCEDURE selectKontakt('$ID_Kontakti')");
+while($res = mysqli_fetch_array($result))
+{
+	$Emri = $res['Emri'];
+	$Email = $res['Email'];
+	$Tel = $res['Tel'];
+	$Mesazhi= $res['Mesazhi'];
+}
+?>
+
+
 <!DOCTYPE HTML>
 <html>
 <head>
 <title>UTK</title>
 <link href="css/bootstrap.css" rel='stylesheet' type='text/css' />
 <link rel="stylesheet" href="css/lightbox.css">
-<link rel="stylesheet" type="text/css" href="css/style2.css">
 <!-- Custom Theme files -->
 <link href="css/style.css" rel='stylesheet' type='text/css' />
+<link rel="stylesheet" type="text/css" href="css/style2.css">
 <!-- Custom Theme files -->
 <!--//theme style-->
 <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -51,9 +103,9 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 					 <li><a href="#">Home</a></li>
 					 <li><a href="#">About</a></li>
 					 <li class="active dropdown">
-						<a href="#" class="scroll dropdown-toggle" data-toggle="dropdown">Kontakti<b class="caret"></b></a>
+						<a href="#" class="scroll dropdown-toggle" data-toggle="dropdown">Kontakt<b class="caret"></b></a>
 						<ul class=" dropdown-menu">
-							<li><a class="active"  href="kontakt.php">Kontaktet</a></li>
+							<li><a  href="kontakt.php">Kontakti</a></li>
 							<li><a  href="suggest.php">Sygjerimet</a></li>
 						</ul>
 					 </li>					
@@ -73,64 +125,54 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 <br>
 <br>
 <!---->
-<div class="contact">
-	 <div class="container">
-	 	<div class="choose d-choose">
-			
-				<h3 class="t-h3">Forma për modifikimin e të dhënave të kontaktit</h3>
-           
-            <div class="d-form" >
-                <form action="" method="post" class=" footer-bottom ">
-                	<div style="width: 70%; text-align: center; margin:0 15% 2% 15%;">
-					    <div class="input-group">
-					      <input type="text" name="term" class="form-control" placeholder="Emri ose Email-i">
-					      <span class="input-group-btn">
-					        <button class="btn btn-default" type="submit">Kërko</button>
-					      </span>
-					    </div><!-- /input-group -->
-  					</div><!-- /.col-lg-6 -->
-			</div>
-					<div class="table-responsive" style="width: 100%;">
-                    <table class="table" style="width: 100%;">
-                        <thead>
-                        	<tr>
-	                            <th class="t-th">Emri dhe Mbiemri</th>
-	                            <th class="t-th">Nr. i Telefonit</th>
-	                            <th class="t-th">Email</th>
-	                            <th class="t-th">Mesazhi</th>
-	                            <th class="t-th">Modifiko</th>
-                            </tr>
-                        </thead>
-                        <tbody style="line-height: 0;text-align: center;">
-                        	<?php
-								if (!empty($_REQUEST['term'])) {
-								$term = ($_REQUEST['term']);     
-								$sql = mysqli_query($conn,"CALL sterm('$term');");
-								while($row = mysqli_fetch_array($sql)) { 		
-										echo "<tr>";
-										echo "<td>".$row['Emri']."</td>";
-										echo "<td>".$row['Email']."</td>";
-										echo "<td>".$row['Tel']."</td>";
-										echo "<td>".$row['Mesazhi']."</td>";	
-										echo "<td><a href=\"update_Kontakt.php?ID_Kontakti=$row[ID_Kontakti]\" class='contact-but-green' style='text-decoration:none;' type='submit'>
-										Modifiko</a></td></tr>";		
-						}
+<div class="container">	
+<div class="choose d-choose">
+<h3 class="t-h3">Forma për modifikimin e Kontakteve</h3>
+<div class="container">		 
+<div class="div-form">
+<form method="post" Kontakti="form1" action="update_Kontakt.php" class=" footer-bottom ">
+<div class="table-responsive">
+<table class="table">
+    <tbody>
+    <tr>
+        <td class="t-td">Emri dhe Mbiemri: </td>
+        <td><input class="form-control" type="text" name="Emri" value='<?php echo $Emri;?>'></td>
+        
+    </tr>
+    <tr>
+        <td class="t-td">Email-i:</td>
+        <td><input class="form-control" type="text" name="Email" value='<?php echo $Email;?>'></td>
+        
+    </tr>
+    <tr>
+        <td class="t-td" >Nr. i Telefonit:
+        	<br><small>Shembull: 123-45-678</small></td>
+        <td><input class="form-control" type="tel" pattern="[0-9]{3}-[0-9]{3}-[0-9]{3}" required name="Tel" value='<?php echo $Tel;?>'>
 
-					}
+        </td>
+        
+    </tr>
+    <tr>
+        <td class="t-td">Mesazhi:</td>
+        <td><textarea class="form-control txt-area" name="Mesazhi" rows="5" ><?php echo $Mesazhi;?></textarea></td>
+        
+    </tr>
+    <tr>
+    	<td><input type="hidden" name="ID_Kontakti" value='<?php echo $_GET['ID_Kontakti'];?>' /></td>
+    	<td><input class="contact-but-blue" name="update" type="submit" value="Modifiko" /></td>
+    </tr>
+</tbody>
+</table>
+</div>                   
+<div >
+</div>
 
-					?>
-                            
-	                    </tbody>
-	                </table>
-	                </div>                   
-                    
-                
-                </form>
-                <div class="clearfix"></div>
-			</div>
-			
-		 </div>
-	 </div>
+</form>
+<div class="clearfix"></div>
+</div>
+
+
+</div>
 </div>
 
 <!-- footer -->
