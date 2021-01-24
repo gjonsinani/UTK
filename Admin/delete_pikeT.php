5df3aa65715a003ea43a1f34a9a269d2704b3d2c
Uranik Sejdiu <u.sejdiu4@gmail.com>
2021-01-24 14:01:42 +0100

    quick fix

--- Admin/delete_pikeT.php
+++ Admin/delete_pikeT.php
@@ -1,9 +1,21 @@
 <?php
-include("config.php");
+require_once "connection.php";
 
-$ID_pikaT = $_GET['ID_pikaT'];
-
-$result = mysqli_query($conn,"DELETE FROM pikatturistike WHERE ID_pikaT=$ID_pikaT");
-
-header("Location:fshi_pikeT.php");
+$id=$_REQUEST['ID_pikaT'];	//get delete_id and store in $id variable
+		
+		$select_stmt= $db->prepare('SELECT * FROM pikatturistike WHERE ID_pikaT =:id');	//sql select query
+		$select_stmt->bindParam(':id',$id);
+		$select_stmt->execute();
+		$row=$select_stmt->fetch(PDO::FETCH_ASSOC);
+		unlink("upload/".$row['Foto1']); //unlink function permanently remove your file
+		unlink("upload/".$row['Foto2']);
+		unlink("upload/".$row['Foto3']);
+		unlink("upload/".$row['Foto4']);
+		
+		//delete an orignal record from db
+		$delete_stmt = $db->prepare('DELETE FROM pikatturistike WHERE ID_pikaT =:id');
+		$delete_stmt->bindParam(':id',$id);
+		$delete_stmt->execute();
+		
+		header("Location:fshi_pikeT.php");
 ?>
